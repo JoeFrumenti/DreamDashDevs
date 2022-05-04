051baa2ac84f9f562f77974cfa734af2a181838e
JoeFrumenti <92900273+JoeFrumenti@users.noreply.github.com>
2022-05-03 22:42:23 -0700

    Added scrolling background, also alora added to instructions

---
+++ DreamDash/Assets/Scripts/Background/Scrolling.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-10 * Time.deltaTime, 0, 0);
+
+        if(transform.position.x < -26.11f)
+        {
+            print(transform.position.x);
+            transform.position = new Vector3(26f, transform.position.y, transform.position.z);
+            print(transform.position.x);
+        }
+    }
+}
